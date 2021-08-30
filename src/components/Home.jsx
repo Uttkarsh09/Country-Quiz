56af3d5b3c415f916eb2231dc43990eee490bde9
Uttkarsh09 <uttkarshpatel9@gmail.com>
2021-08-30 22:18:56 +0530

    made responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,29 +7,29 @@ const Home = () => {
 	const [countryInfo, setCountryInfo] = useState(null);
 
 	useEffect(() => {
-		// function getCountriesInfo() {
-		// 	axios
-		// 		.request({
-		// 			url: "/all?fields=name;currencies;capital;languages;borders;flag",
-		// 			method: "get",
-		// 			baseURL: "https://restcountries.eu/rest/v2/",
-		// 		})
-		// 		.then((res) => {
-		// 			setCountryInfo(res.data);
-		// 		});
-		// }
-		async function getCountriesInfo() {
-			axios.get("http://127.0.0.1:9999/").then((res) => {
-				setCountryInfo(res.data);
-			});
+		function getCountriesInfo() {
+			axios
+				.request({
+					url: "/all?fields=name;currencies;capital;languages;borders;flag",
+					method: "get",
+					baseURL: "https://restcountries.eu/rest/v2/",
+				})
+				.then((res) => {
+					setCountryInfo(res.data);
+				});
 		}
 		getCountriesInfo();
-		// readCountryData();
 	}, []);
 
 	return (
 		<div className="home">
 			{countryInfo ? <QuizContent {...{ countryInfo }} /> : "loading"}
+			<div className="about-me">
+				created by <a href="https://github.com/Uttkarsh09">Uttkarsh09</a> -{" "}
+				<a href="https://devchallenges.io/challenges/Bu3G2irnaXmfwQ8sZkw8">
+					devChallenges.io
+				</a>
+			</div>
 		</div>
 	);
 };
